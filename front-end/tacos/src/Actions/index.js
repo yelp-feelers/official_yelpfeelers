1a59c9fb7a9567641010fe8f2acbe0ffc92b936a
erin-koen <erin.koen@gmail.com>
2019-03-11 14:21:14 -0400

    actions sketched out

---
+++ front-end/tacos/src/Actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+
+// initial actions will be login start and login success
+
+export const LOGIN_START = 'LOGIN_START'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+
+//action creators - login for login component, getTacos for CDM within protected home route
+
+export const login = something => dispatch => {
+
+}
+
+export const getTacos = () => dispatch => {
+    //axios request
+}
