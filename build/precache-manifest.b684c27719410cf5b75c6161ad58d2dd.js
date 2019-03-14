930c4da256746787dfb5292ff47ac85ccaf18d42
erin-koen <erin.koen@gmail.com>
2019-03-14 15:01:39 -0400

    new front-end directory added

---
+++ build/precache-manifest.b684c27719410cf5b75c6161ad58d2dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "515cd9befd3e3c1df06f",
+    "url": "/static/css/main.78ddf747.chunk.css"
+  },
+  {
+    "revision": "515cd9befd3e3c1df06f",
+    "url": "/static/js/main.53df0087.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebaff9d8ae0b2ae0790d",
+    "url": "/static/css/2.639462bd.chunk.css"
+  },
+  {
+    "revision": "ebaff9d8ae0b2ae0790d",
+    "url": "/static/js/2.70f59f3d.chunk.js"
+  },
+  {
+    "revision": "a159ca5896e4811fb44836d9c7e1c800",
+    "url": "/static/media/taco.a159ca58.png"
+  },
+  {
+    "revision": "854daad695842cdcb96e43e3ea1096a1",
+    "url": "/static/media/taco3.854daad6.png"
+  },
+  {
+    "revision": "c670f29bfcd3e07b17b79ac42b93e79c",
+    "url": "/static/media/taco4.c670f29b.png"
+  },
+  {
+    "revision": "4645de2fec7cf56f4978b331a72ea2de",
+    "url": "/static/media/background.4645de2f.jpg"
+  },
+  {
+    "revision": "e0e847e49af64d8cdd988a82328592fc",
+    "url": "/index.html"
+  }
+];
